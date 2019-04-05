5824e0a07634329824ec10a7efe91f9bbd5fc50b
erzqy <riezqy189@gmail.com>
2019-04-05 21:30:59 +0800

    nothing

--- autolike-facebook.js
+++ autolike-facebook.js
@@ -1,9 +1,61 @@
+var liker = null;
+
+
+function tulis(argument) {
+	console.log(argument);
+}
 
 function likeGenerator() {
 	io = 0; // initilizing number liked
-	
-	// $$ is a Selector by facebook like a jquery $
-	$$('a[data-testid="UFI2ReactionLink"]').forEach(
+
+	// $$ is a Selector by firefox like a jquery $
+	notliked = $$('a[data-testid="UFI2ReactionLink"]');
+
+	if (document.querySelector('#boxInfo') == undefined) {
+
+		boxInfo = document.createElement('div');
+		boxInfo.style = 'background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:100px;height:60px;border:4px solid black;z-index:9999;padding-top:15px;';
+		boxInfo.id = 'boxInfo';
+
+		liked = document.createElement('span');
+		liked.innerHTML = 0;
+		liked.id = 'liked';
+
+		alllike = document.createElement('span');
+		alllike.id = 'alllike';
+		alllike.innerHTML = 0;
+
+		buttonNav = document.createElement('button');
+		buttonNav.id = 'likecontrol';
+		buttonNav.style = 'margin-top:12px;'
+		buttonNav.innerHTML = 'state !';
+		buttonNav.onclick = function(event){
+			changeListener();
+		};
+
+		boxInfo.appendChild(liked);
+		boxInfo.appendChild(document.createTextNode(' of '));
+		boxInfo.appendChild(alllike);
+		boxInfo.appendChild(buttonNav);
+
+
+		document.getElementsByTagName('body')[0].appendChild(boxInfo);
+	}	
+
+	counted = 0;
+	notliked.forEach(
+	  function(val){
+	    
+	    // checking color of liked post ot not
+	    if (val.style.color !== "rgb(53, 120, 229)"){
+	    	counted++;
+      }
+    });	
+
+  alllike = document.querySelector('#alllike');
+  alllike.innerHTML = parseInt(alllike.innerHTML) + counted;
+
+	notliked.forEach(
 	  function(val){
 	    
 	    // checking color of liked post ot not
@@ -14,37 +66,37 @@ function likeGenerator() {
         val.click();
 
        // printing to log
-       console.log(String(Date()) + " - " + String(io));
 
-       if (document.querySelector('#boxInfo') == undefined) {
-       	boxInfo = $$('<div></div>')
-       		.append($$('<div></div>')
-       			.attr('id', '')
-       			.attr('', '')
-       			.attr('', '')
-       			)
-				boxInfo.innerHTML = 
-				'<div id=\'happy\' style=\'background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:200px;height:100px;border:4px solid black;z-index:9999;padding-top:15px;\'>
-				<span>0</span>
-				 of ' + happy.length + ' items liked.
+       liked = document.querySelector('#liked');
+		   liked.innerHTML = parseInt(liked.innerHTML) + 1;
 
-				 <div id=\'happyStatus\' style=\'margin-top:30px;\'><a id=\'happyButton\' href=\'#\' style=\'display:block;\' onclick=\'haltFn();\'>Stop it.</a></div></div>'
-				document.getElementsByTagName('body')[0].appendChild(boxInfo);
-       }
+		   setInterval(function(){
+	       tulis(String(Date()) + " - " + String(io));
+		   }, 2000)
 
       }
     });	
+
+	notliked = null;
 }
 
-function stopListener(thisis) {
-	clearInteval(thisis);
+function changeListener() {
+
+	if (liker === null) {
+		tulis('creating listener.');
+
+		liker = setInterval(function(){
+				likeGenerator();
+		    window.scrollBy(0,50);
+			},
+			  
+			  // interval 10 seconds
+			  10000
+			);		
+	} else {
+		tulis('removing listener.')
+		clearInterval(liker);
+	}
 }
-// I Love You
 
-liker = setInterval(function(){
-
-	},
-	  
-	  // interval 10 seconds
-	  10000
-	);
+changeListener();
